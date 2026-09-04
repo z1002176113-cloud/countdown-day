02e3b95cfeaffd62e4f98c604894bfcf4435bdc5
Xiangx2 <Xiangx2@noreply.gitcode.com>
2026-09-04 14:29:46 +0800

    fix: 添加 metro.config.js 配置 @/ 路径别名解析

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  '@': path.resolve(__dirname, 'src'),
+};
+
+module.exports = config;
